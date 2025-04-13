e4619bed75b269f80591cbf4cac779e8d2137ea8
Pato05 <pato05mc@gmail.com>
2025-04-13 19:20:18 +0200

    chore: example app

---
+++ example/android/app/src/main/kotlin/com/example/just_audio_media_kit_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.just_audio_media_kit_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <media_kit_libs_windows_audio/media_kit_libs_windows_audio_plugin_c_api.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  MediaKitLibsWindowsAudioPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("MediaKitLibsWindowsAudioPluginCApi"));
+}
